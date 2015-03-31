54886b456ade209147c0ff760212e26068803ad3
jshields91 <jeshie02@louisville.edu>
2015-03-31 15:45:39 -0400

    cookie nav and some routes
    created cookie to check if user has visited the page before.
    created nav bar.
    initialized beginning routes.

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-﻿
+﻿(function () {
+    var app = angular.module('mainApp', ['ngRoute'])
+    .config(['$routeProvider',
+        function ($routeProvider) {
+            $routeProvider.
+                when('/Intro', {
+                    templateUrl: 'intro.html'
+
+                }).
+                when('/WebDev', {
+                    templateUrl: 'webdev.html'
+
+                }).
+                when('/SQL', {
+                    templateUrl: 'sql.html'
+                }).
+                when('/Analyst', {
+                    templateUrl: 'analyst.html'
+
+                }).
+                when('/Infrastructure', {
+                    templateUrl: 'infrastructure.html'
+
+                }).
+                when('/', {
+                    templateUrl: 'bio.html'
+
+                }).
+                otherwise({
+                    redirectTo: '/'
+                });
+    }]);
+
+    
+
+    app.controller('TabController', function () {
+        this.tab = 0;
+
+        this.setTab = function (tab) {
+            this.tab = tab;
+        };
+
+        this.isSet = function (tab) {
+            return (this.tab === tab);
+        };
+    });
+
+})();
